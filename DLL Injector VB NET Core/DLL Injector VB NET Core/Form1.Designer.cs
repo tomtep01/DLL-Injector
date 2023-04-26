9d67c3fd43facadcfd0566de07f42eaf9c102307
tomtep01 <testacckk515@gmail.com>
2023-04-26 19:01:57 +0700

    Built in C#, not VB, I accidentally renamed it.

---
+++ DLL Injector VB NET Core/DLL Injector VB NET Core/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace DLL_Injector_VB_NET_Core
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listBoxProcesses = new System.Windows.Forms.ListBox();
+            label1 = new System.Windows.Forms.Label();
+            labelProcessSelected = new System.Windows.Forms.Label();
+            textBox1 = new System.Windows.Forms.TextBox();
+            button1 = new System.Windows.Forms.Button();
+            button2 = new System.Windows.Forms.Button();
+            listBoxDlls = new System.Windows.Forms.ListBox();
+            label2 = new System.Windows.Forms.Label();
+            labelSelectedDll = new System.Windows.Forms.Label();
+            button3 = new System.Windows.Forms.Button();
+            button4 = new System.Windows.Forms.Button();
+            DllsPath = new System.Windows.Forms.ListBox();
+            label3 = new System.Windows.Forms.Label();
+            button5 = new System.Windows.Forms.Button();
+            SuspendLayout();
+            // 
+            // listBoxProcesses
+            // 
+            listBoxProcesses.FormattingEnabled = true;
+            listBoxProcesses.ItemHeight = 15;
+            listBoxProcesses.Location = new System.Drawing.Point(1, 31);
+            listBoxProcesses.Name = "listBoxProcesses";
+            listBoxProcesses.Size = new System.Drawing.Size(372, 244);
+            listBoxProcesses.TabIndex = 0;
+            listBoxProcesses.SelectedIndexChanged += listBoxProcesses_SelectedIndexChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            label1.Location = new System.Drawing.Point(1, 278);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(70, 21);
+            label1.TabIndex = 1;
+            label1.Text = "Process :";
+            // 
+            // labelProcessSelected
+            // 
+            labelProcessSelected.AutoSize = true;
+            labelProcessSelected.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            labelProcessSelected.Location = new System.Drawing.Point(68, 278);
+            labelProcessSelected.Name = "labelProcessSelected";
+            labelProcessSelected.Size = new System.Drawing.Size(123, 21);
+            labelProcessSelected.TabIndex = 2;
+            labelProcessSelected.Text = "Select a process.";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new System.Drawing.Point(1, 2);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new System.Drawing.Size(235, 23);
+            textBox1.TabIndex = 3;
+            textBox1.Text = "Search";
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // button1
+            // 
+            button1.Location = new System.Drawing.Point(242, 5);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(131, 23);
+            button1.TabIndex = 4;
+            button1.Text = "Refresh";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new System.Drawing.Point(713, 5);
+            button2.Name = "button2";
+            button2.Size = new System.Drawing.Size(75, 23);
+            button2.TabIndex = 5;
+            button2.Text = "Add DLL";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // listBoxDlls
+            // 
+            listBoxDlls.FormattingEnabled = true;
+            listBoxDlls.ItemHeight = 15;
+            listBoxDlls.Location = new System.Drawing.Point(416, 31);
+            listBoxDlls.Name = "listBoxDlls";
+            listBoxDlls.Size = new System.Drawing.Size(385, 244);
+            listBoxDlls.TabIndex = 6;
+            listBoxDlls.SelectedIndexChanged += listBoxDlls_SelectedIndexChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            label2.Location = new System.Drawing.Point(416, 278);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(44, 21);
+            label2.TabIndex = 7;
+            label2.Text = "DLL :";
+            // 
+            // labelSelectedDll
+            // 
+            labelSelectedDll.AutoSize = true;
+            labelSelectedDll.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            labelSelectedDll.Location = new System.Drawing.Point(457, 278);
+            labelSelectedDll.Name = "labelSelectedDll";
+            labelSelectedDll.Size = new System.Drawing.Size(97, 21);
+            labelSelectedDll.TabIndex = 8;
+            labelSelectedDll.Text = "Select a DLL.";
+            // 
+            // button3
+            // 
+            button3.Location = new System.Drawing.Point(576, 5);
+            button3.Name = "button3";
+            button3.Size = new System.Drawing.Size(131, 23);
+            button3.TabIndex = 9;
+            button3.Text = "Remove Selected DLL";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Font = new System.Drawing.Font("Segoe UI", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            button4.Location = new System.Drawing.Point(3, 305);
+            button4.Name = "button4";
+            button4.Size = new System.Drawing.Size(798, 147);
+            button4.TabIndex = 10;
+            button4.Text = "Inject!";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // DllsPath
+            // 
+            DllsPath.FormattingEnabled = true;
+            DllsPath.ItemHeight = 15;
+            DllsPath.Location = new System.Drawing.Point(600, 31);
+            DllsPath.Name = "DllsPath";
+            DllsPath.Size = new System.Drawing.Size(201, 244);
+            DllsPath.TabIndex = 11;
+            DllsPath.Visible = false;
+            DllsPath.SelectedIndexChanged += DllsPath_SelectedIndexChanged;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            label3.Location = new System.Drawing.Point(197, 278);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(35, 21);
+            label3.TabIndex = 12;
+            label3.Text = "hey";
+            label3.Visible = false;
+            // 
+            // button5
+            // 
+            button5.Location = new System.Drawing.Point(439, 5);
+            button5.Name = "button5";
+            button5.Size = new System.Drawing.Size(131, 23);
+            button5.TabIndex = 13;
+            button5.Text = "Remove All DLLs";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(800, 450);
+            Controls.Add(button5);
+            Controls.Add(label3);
+            Controls.Add(DllsPath);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(labelSelectedDll);
+            Controls.Add(label2);
+            Controls.Add(listBoxDlls);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(labelProcessSelected);
+            Controls.Add(label1);
+            Controls.Add(listBoxProcesses);
+            Name = "Form1";
+            Text = "DLL Injector";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxProcesses;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelProcessSelected;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBoxDlls;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelSelectedDll;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox DllsPath;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button5;
+    }
+}
---
+++ DLL Injector VB NET Core/DLL Injector VB NET Core/Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace DLL_Injector_VB_NET_Core
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Ansi, SetLastError = true)]
+        private static extern IntPtr GetProcAddress(IntPtr hModule, string lpProcName);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress, uint dwSize, uint flAllocationType, uint flProtect);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, uint nSize, out UIntPtr lpNumberOfBytesWritten);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool CloseHandle(IntPtr hObject);
+
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr CreateRemoteThread(IntPtr hProcess, IntPtr lpThreadAttribute, uint dwStackSize, IntPtr lpStartAddress,
+                                                        IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
+
+        private const int PROCESS_CREATE_THREAD = 0x0002;
+        private const int PROCESS_QUERY_INFORMATION = 0x0400;
+        private const int PROCESS_VM_OPERATION = 0x0008;
+        private const int PROCESS_VM_WRITE = 0x0020;
+        private const int PROCESS_VM_READ = 0x0010;
+        private const uint MEM_COMMIT = 0x00001000;
+        private const uint MEM_RESERVE = 0x00002000;
+        private const uint PAGE_READWRITE = 0x04;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.PerformClick();
+            foreach (Process p in Process.GetProcesses())
+            {
+                listBoxProcesses.Items.Add(p.ProcessName + " (ID: " + p.Id.ToString() + ")");
+            }
+        }
+
+        private void listBoxProcesses_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBoxProcesses.SelectedItem != null)
+            {
+                string selectedItem = listBoxProcesses.SelectedItem.ToString();
+                int idStartIndex = selectedItem.IndexOf("(ID: ") + 5;
+                int idEndIndex = selectedItem.Length - 1;
+                int processId = int.Parse(selectedItem.Substring(idStartIndex, idEndIndex - idStartIndex));
+
+                string processName = selectedItem.Substring(0, idStartIndex - 6); // Extract the process name
+                string selectedProcess = processName + " (ID: " + processId.ToString() + ")"; // Merge the process name and ID into a single string
+
+                labelProcessSelected.Text = selectedProcess; // Display the selected process in the TextBox
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            listBoxProcesses.Items.Clear();
+
+            foreach (Process p in Process.GetProcesses())
+            {
+                string processName = p.ProcessName + " (ID: " + p.Id.ToString() + ")";
+                if (string.IsNullOrEmpty(textBox1.Text) || processName.Contains(textBox1.Text, StringComparison.OrdinalIgnoreCase))
+                {
+                    listBoxProcesses.Items.Add(processName);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBoxProcesses.Items.Clear();
+
+            foreach (Process p in Process.GetProcesses())
+            {
+                listBoxProcesses.Items.Add(p.ProcessName + " (ID: " + p.Id.ToString() + ")");
+            }
+
+            labelProcessSelected.Text = string.Empty;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "DLL files (*.dll)|*.dll";
+            openFileDialog.Title = "Select a DLL file";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+
+                foreach (string file in openFileDialog.FileNames)
+                {
+                    string dllName = Path.GetFileName(openFileDialog.FileName);
+                    listBoxDlls.Items.Add(dllName);
+                    DllsPath.Items.Add(openFileDialog.FileName);
+                }
+            }
+        }
+
+        private void listBoxDlls_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBoxDlls.SelectedItem != null)
+            {
+                labelSelectedDll.Text = listBoxDlls.SelectedItem.ToString();
+                if (listBoxDlls.SelectedIndex != DllsPath.SelectedIndex)
+                {
+                    DllsPath.SelectedIndex = listBoxDlls.SelectedIndex;
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listBoxDlls.SelectedItem != null)
+            {
+                listBoxDlls.Items.Remove(listBoxDlls.SelectedItem);
+                labelSelectedDll.Text = "";
+                if (DllsPath.SelectedItem != null)
+                {
+                    listBoxDlls.Items.Remove(listBoxDlls.SelectedItem);
+                    label3.Text = "";
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string dllPath = label3.Text;
+            if (dllPath == "")
+            {
+                MessageBox.Show("Please select a DLL file to inject", "Error");
+                return;
+            }
+
+            if (listBoxProcesses.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please select a process to inject into", "Error");
+                return;
+            }
+
+            string processName = listBoxProcesses.SelectedItem.ToString().Split('(')[0].Trim();
+            Process[] processes = Process.GetProcessesByName(processName);
+            if (processes.Length == 0)
+            {
+                MessageBox.Show("Unable to find process: " + processName, "Error");
+                return;
+            }
+
+            int processId = processes[0].Id;
+            IntPtr processHandle = OpenProcess(PROCESS_CREATE_THREAD | PROCESS_QUERY_INFORMATION | PROCESS_VM_OPERATION | PROCESS_VM_WRITE | PROCESS_VM_READ, false, processId);
+            if (processHandle == IntPtr.Zero)
+            {
+                MessageBox.Show("Unable to open process: " + processName, "Error");
+                return;
+            }
+
+            IntPtr loadLibraryAddr = GetProcAddress(GetModuleHandle("kernel32"), "LoadLibraryA");
+            if (loadLibraryAddr == IntPtr.Zero)
+            {
+                MessageBox.Show("Unable to find LoadLibraryA function in kernel32.dll", "Error");
+                return;
+            }
+
+            byte[] dllPathBytes = Encoding.ASCII.GetBytes(dllPath);
+            IntPtr dllPathAddress = VirtualAllocEx(processHandle, IntPtr.Zero, (uint)dllPathBytes.Length, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+            if (dllPathAddress == IntPtr.Zero)
+            {
+                MessageBox.Show("Unable to allocate memory in remote process", "Error");
+                return;
+            }
+
+            UIntPtr bytesWritten;
+            if (!WriteProcessMemory(processHandle, dllPathAddress, dllPathBytes, (uint)dllPathBytes.Length, out bytesWritten))
+            {
+                MessageBox.Show("Unable to write to remote process memory", "Error");
+                return;
+            }
+
+            IntPtr threadHandle = CreateRemoteThread(processHandle, IntPtr.Zero, 0, loadLibraryAddr, dllPathAddress, 0, IntPtr.Zero);
+            if (threadHandle == IntPtr.Zero)
+            {
+                MessageBox.Show("Unable to create remote thread in target process", "Error");
+                return;
+            }
+
+            MessageBox.Show("DLL injected successfully!", "Success");
+
+            CloseHandle(processHandle);
+            CloseHandle(threadHandle);
+        }
+
+        private void DllsPath_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedDll = (string)DllsPath.SelectedItem;
+            label3.Text = selectedDll;
+            if (DllsPath.SelectedIndex != listBoxDlls.SelectedIndex)
+            {
+                listBoxDlls.SelectedIndex = DllsPath.SelectedIndex;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            listBoxDlls.Items.Clear();
+            DllsPath.Items.Clear();
+        }
+    }
+}
